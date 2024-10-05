2d6fa6155d822158f3c6456f04b58865188c6014
Jose Bustos <35240264+korbek99@users.noreply.github.com>
2024-10-05 20:05:07 -0300

    Initial Commit

---
+++ RentaCards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RentaCards
+//
+//  Created by Jose Preatorian on 05-10-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
