9561a2764846467e76b6aa56277473a9dd00220d
Jose Bustos <35240264+korbek99@users.noreply.github.com>
2024-10-09 00:34:14 -0300

    adding menu

--- RentaCards/Views/HomeViewController.swift
+++ RentaCards/Views/HomeViewController.swift
@@ -9,12 +9,112 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    let menuView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .systemGray
+        view.layer.shadowOpacity = 1
+        view.layer.shadowRadius = 6
+        return view
+    }()
+    
+    var leadingConstraint: NSLayoutConstraint!
+    var menuShowing = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Home"
-        // Do any additional setup after loading the view.
+        setupUX()
+        setupNavigationBar()
+        setupMenuView()
     }
 
 
+    func setupUX() {
+        view.backgroundColor = .white
+    
+        view.addSubview(menuView)
+//        view.addSubview(tableView)
+//
+//        NSLayoutConstraint.activate([
+//            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+//            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+//            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+//        ])
+    }
+    
+    func setupMenuView() {
+        leadingConstraint = menuView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -200)
+        view.addSubview(menuView)
+
+        NSLayoutConstraint.activate([
+            leadingConstraint,
+            menuView.topAnchor.constraint(equalTo: view.topAnchor),
+            menuView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            menuView.widthAnchor.constraint(equalToConstant: 200)
+        ])
+        
+        let profileImageView = UIImageView()
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        profileImageView.image = UIImage(systemName: "person.circle.fill")
+        profileImageView.contentMode = .scaleAspectFit
+        profileImageView.tintColor = .white
+        menuView.addSubview(profileImageView)
+
+        let welcomeLabel = UILabel()
+        welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
+        welcomeLabel.text = "Bienvenido"
+        welcomeLabel.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        welcomeLabel.textColor = .white
+        menuView.addSubview(welcomeLabel)
+
+        let navigateButton = UIButton(type: .system)
+        navigateButton.translatesAutoresizingMaskIntoConstraints = false
+        navigateButton.setTitle("Config", for: .normal)
+        navigateButton.setTitleColor(.white, for: .normal)
+        navigateButton.addTarget(self, action: #selector(navigateToNextScreen), for: .touchUpInside)
+        menuView.addSubview(navigateButton)
+        
+        let navigateLogin = UIButton(type: .system)
+        navigateLogin.translatesAutoresizingMaskIntoConstraints = false
+        navigateLogin.setTitle("Log Out", for: .normal)
+        navigateLogin.setTitleColor(.white, for: .normal)
+//        navigateLogin.addTarget(self, action: #selector(navigateToLogin), for: .touchUpInside)
+        menuView.addSubview(navigateLogin)
+
+        NSLayoutConstraint.activate([
+            profileImageView.centerXAnchor.constraint(equalTo: menuView.centerXAnchor),
+            profileImageView.topAnchor.constraint(equalTo: menuView.topAnchor, constant: 160),
+            profileImageView.widthAnchor.constraint(equalToConstant: 80),
+            profileImageView.heightAnchor.constraint(equalToConstant: 80),
+            
+            welcomeLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 10),
+            welcomeLabel.centerXAnchor.constraint(equalTo: menuView.centerXAnchor),
+            
+            navigateButton.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 20),
+            navigateButton.centerXAnchor.constraint(equalTo: menuView.centerXAnchor),
+            
+            navigateLogin.topAnchor.constraint(equalTo: navigateButton.bottomAnchor, constant: 20),
+            navigateLogin.centerXAnchor.constraint(equalTo: menuView.centerXAnchor)
+        ])
+    }
+    
+    func setupNavigationBar() {
+        let menuImage = UIImage(systemName: "line.horizontal.3")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: menuImage, style: .plain, target: self, action: #selector(menuAction))
+    }
+
+    @objc func menuAction() {
+        leadingConstraint.constant = menuShowing ? -200 : 0
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+        menuShowing.toggle()
+    }
+    
+    @objc func navigateToNextScreen() {
+        // Navegar a la siguiente pantalla
+    }
 }
 
